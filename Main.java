4950ab8d567fa6eaf84cbff5aa732ba70c06fc62
Sazia <sazianagma@gmail.com>
2023-01-27 21:33:35 +0530

    git commit "first commit"

--- Main.java
+++ Main.java
@@ -2,5 +2,7 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello world!");
+        System.out.println("bye bye java");
+        System.out.println("hello");
     }
 }
